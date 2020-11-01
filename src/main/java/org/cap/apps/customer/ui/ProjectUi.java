4304bf5b60b15d199ffb57ccb9d1e62e26444cd0
nihaksh <nihakshirasagar@gmail.com>
2020-10-31 19:43:50 -0700

    customerspring

--- src/main/java/org/cap/apps/customer/ui/ProjectUi.java
+++ src/main/java/org/cap/apps/customer/ui/ProjectUi.java
@@ -15,16 +15,16 @@ public class ProjectUi {
 
 	public void run()
 	{
-		Customer customer = add(1L, "tushar");
-		Customer customer1 = add(2L, "Gaurav");
-		Customer customer2= add(3L, "kabir");
+		Customer customer = add(1L, "panda");
+		Customer customer1 = add(2L, "sonu");
+		Customer customer2= add(3L, "rashu");
 		Long id1 = customer.getId();
 		System.out.println("Before update: "+customer.toString());
-		customer = update(id1, "gaurav");
+		customer = update(id1, "sonu");
 		System.out.println("After update: "+customer.toString());
 		System.out.println("Before update: "+customer1.toString());
 		Long id2 = customer1.getId();
-		customer1 = update(id2, "tushar");
+		customer1 = update(id2, "panda");
 		System.out.println("After update: "+customer1.toString());
 		System.out.println(customer2.toString());
 	}
